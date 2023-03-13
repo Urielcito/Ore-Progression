18aa228ce8b8ac69436f73661757bb8f65456bfd
Uriel Larrea <kuroshiromugen@gmail.com>
2023-03-13 00:41:22 -0300

    Todos los lingotes terminados con texturas y todo el mod traducido al español, no voy a traducirlo a Inglés por un buen tiempo

--- src/main/java/net/kuroshiro/oreprogression/item/ModItems.java
+++ src/main/java/net/kuroshiro/oreprogression/item/ModItems.java
@@ -11,6 +11,18 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, OreProgression.MOD_ID);
     public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> QUALITY_COPPER_INGOT = ITEMS.register("quality_copper_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> LEAD_INGOT = ITEMS.register("lead_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> QUALITY_IRON_INGOT = ITEMS.register("quality_iron_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> PLATINUM_INGOT = ITEMS.register("platinum_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> QUALITY_GOLD_INGOT = ITEMS.register("quality_gold_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> CRIMTANE_INGOT = ITEMS.register("crimtane_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+    public static final RegistryObject<Item> DEMONITE_INGOT = ITEMS.register("demonite_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+
+    public static final RegistryObject<Item> HELLSTONE_INGOT = ITEMS.register("hellstone_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OREPROGRESSION_TAB)));
+
 
     //TO-DO: ITEMS DE CADA ORE, Y SUS RESPECTIVOS LINGOTES!!
     public static void register(IEventBus eventBus){
